b44ec0baeacd4e2247f07e4335c9588cd60a2345
Jose Torres <vidojam@gmail.com>
2024-07-09 19:09:28 -0400

    worked on ghosts, blinky, inky, pinky  & cydle

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
   let score = 0
   const grid = document.querySelector('.grid')
 
-
 const layout = [
   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
   1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
@@ -85,18 +84,131 @@ function movePacman(e) {
   squares[pacmanCurrentIndex].classList.remove('pac-man')
   //console.log(e.key)
   switch (e.key) {
+
     case 'ArrowLeft':
-      pacmanCurrentIndex -=1
-      break
+      if (
+        pacmanCurrentIndex % width !==0 && 
+        !squares[pacmanCurrentIndex -1].classList.contains('wall') &&
+        !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair') 
+      ){
+        pacmanCurrentIndex -=1
+        break
+      }
+      if(squares[pacmanCurrentIndex] ===squares [363]) 
+        { pacmanCurrentIndex = 391
+
+      }
+
+      case 'ArrowUp':
+        if (
+          pacmanCurrentIndex  - width >= 0 &&
+          !squares[pacmanCurrentIndex -1].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair') 
+        ) [
+          pacmanCurrentIndex -= width
+        ]
+
+       
+        break
+   
     case 'ArrowRight':
-      pacmanCurrentIndex +=1
+      if(
+        pacmanCurrentIndex % width < width &&
+        !squares[pacmanCurrentIndex +1].classList.contains('wall') &&
+        !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair') 
+      ) {
+        pacmanCurrentIndex +=1
+      }
+
+      if(squares[pacmanCurrentIndex] ===squares [392]) 
+        {pacmanCurrentIndex = 364
+      }  
       break
-    case 'ArrowUp':
-      pacmanCurrentIndex -= width
-      break
+      
+      case 'Arrowdown':
+        if(
+          pacmanCurrentIndex % width < width * width &&
+          !squares[pacmanCurrentIndex +1].classList.contains('wall') &&
+          !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair') 
+        ) {
+          pacmanCurrentIndex += width
+        }
+       
+        break
   }
   squares[pacmanCurrentIndex].classList.add('pac-man')
+  // pacDotEaten()
+  // powerPelletEaten()
+  // checkForGameOver()
+  // checkForWin()
 }
 document.addEventListener('keyup', movePacman)
+
+// what happens when you eat a pac-dot
+
+function pacDotEaten() {
+  if (squares[pacmanCurrentIndex].classList.contains('pac-dot')) {
+    score++
+    scoreDisplay.innerHTML = score
+    squares[pacmanCurrentIndex].classList.remove('pac-dot')
+  }
+}
+
+  // what happens when you eat a power-pellet
+  function powerPelletEaten() {
+    if (squares[pacmanCurrentIndex].classList.contains('power-pellet')) {
+      score +=10
+      scoreDisplay.innerHTML = score
+
+
+      squares[pacmanCurrentIndex].classList.remove('power-pellet')
+    }
+  }
+
+  // create ghosts using Constructor
+  class Ghost {
+    constructor(className, startIndex, speed) {
+      this.className = className
+      this.startIndex = startIndex
+      this.speed = speed
+      this.currentIndex = startIndex
+      this.isScared = false
+      this.timerId = NaN
+    }
+  }
+
+  // all my ghost
+
+  ghosts = [
+    new Ghost ('blinky', 348, 250),
+    new Ghost ('pinky', 3376, 400),
+    new Ghost ('inky', 351, 300),
+    new Ghost ('clyde', 379, 500),
+  ]
+
+  console.log (ghosts)
+
+  // draw my ghosts onto the grid
+
+  ghosts.forEach(ghost => {
+    squares[ghost.currentIndex].classList.add(ghost.classname)
+    squares[ghost.currentIndex].classList.add('ghost')
+  })
+
+  // move ghost randomly
+
+   ghosts.forEach(ghost => moveGhost(ghost))
+   
+   function moveGhost(ghost) {
+    console.log(ghost)
+    const directions = [-1, 1, width, -width]
+    const direction = directions[Math.floor(Math.random() * directions.length)]
+
+    ghost.timerId = setInterval(function() {
+      squares[ghost.currentIndex].classList.remove(ghost.className, 'ghost')
+      ghost.currentIndex += direction
+      squares[ghost.currentIndex].classList.add(ghost.className,'ghost')
+    })
+   }
 
 
